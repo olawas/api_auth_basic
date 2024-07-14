dff7cdcf2a56de55aef36089f201fe6ece1dcd2d
CiervoZombie <marcoxcolo@gmail.com>
2024-07-14 18:33:26 -0400

    FirstCommit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,13 +3,38 @@ import UserService from '../services/UserService.js';
 import NumberMiddleware from '../middlewares/number.middleware.js';
 import UserMiddleware from '../middlewares/user.middleware.js';
 import AuthMiddleware from '../middlewares/auth.middleware.js';
-
 const router = Router();
+
+router.post('/bulkCreate', async (req, res) => {
+    try {
+        const { successCount, failureCount } = await UserService.bulkCreateUsers(req.body.users);
+        res.status(200).json({
+            "Creados": successCount,
+            "Fallos": failureCount
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+router.get('/findUsers',[], async (req,res) => {
+    const response = await UserService.findUsers(req);
+    res.status(response.code).json(response.message);
+})
+
+router.get('/getAllUsers', 
+    [
+        
+    ], async (req,res) => {
+    const response = await UserService.getActiveUsers();
+    res.status(response.code).json(response.message);
+
+})
 
 router.post('/create', async (req, res) => {
     const response = await UserService.createUser(req);
     res.status(response.code).json(response.message);
 });
+
 
 router.get(
     '/:id',
--- services/UserService.js
+++ services/UserService.js
@@ -1,5 +1,106 @@
 import db from '../dist/db/models/index.js';
 import bcrypt from 'bcrypt';
+import { Op } from 'sequelize';
+import user from '../dist/db/models/user.js';
+// GET api/v1/users/findUsers que devuelva una lista filtrada de usuarios que
+// cumplan con cualquiera de los siguientes parámetros
+// - eliminados (true or false)
+// - Coincidan parcial o totalmente con el nombre
+// - Hayan iniciado sesión antes de una fecha especificada
+// - Hayan iniciado sesión después de una fecha especificada
+// (Obs: usar los parámetros del request, QueryParams como filtros)
+
+const validateUser = (user) => {
+    const { name, password, email, cellphone } = user;
+    if (!name || !password || !email || !cellphone) {
+        return false;
+    }
+    return true;
+};
+
+const bulkCreateUsers = async (users) => {
+    let successCount = 0;
+    let failureCount = 0;
+
+    for (const user of users) {
+        if (validateUser(user)) {
+            try {
+                const userExist = await db.User.findOne({
+                    where: {
+                        email: user.email
+                    }
+                });
+                if (userExist) {
+                    failureCount++;
+                    return 
+                }
+            
+                const encryptedPassword = await bcrypt.hash(user.password, 10);
+                const newUser = await db.User.create({
+                    name: user.name,
+                    email: user.email,
+                    password: encryptedPassword,
+                    cellphone: user.cellphone,
+                    status: true
+                });
+                if(newUser){
+                    successCount++;
+                }
+            } catch (error) {
+                console.error('Error creating user:', error);
+                failureCount++;
+            }
+        } else {
+            console.log("error con validacion")
+            failureCount++;
+        }
+    }
+
+    return { successCount, failureCount };
+};
+
+const findUsers = async (req) => {
+    const {
+        name,
+        deleted,
+        loginBefore,
+        loginAfter
+    } = req.query; // Usamos req.params en vez de req.query
+
+    const users = await db.User.findAll({
+        where: {
+            name: {
+                [Op.like]: `%${name}%`
+            },
+            status: deleted === 'true' ? false : true
+        }
+    });
+
+    const filteredUsers = await Promise.all(users.map(async (user) => {
+        const sessions = await db.Session.findAll({
+            where: {
+                id_user: user.id,
+                expiration: {
+                    [Op.lte]: new Date(loginBefore),
+                    [Op.gte]: new Date(loginAfter)
+                }
+            }
+        });
+
+        // Retornar el usuario si tiene al menos una sesión en el rango de fechas
+        return sessions.length > 0 ? user : null;
+    }));
+
+    // Filtrar resultados nulos
+    const validUsers = filteredUsers.filter(user => user !== null);
+
+
+    
+    return {
+        code: 200,
+        message: validUsers
+    };
+};
 
 const createUser = async (req) => {
     const {
@@ -102,9 +203,22 @@ const deleteUser = async (id) => {
     };
 }
 
+const getActiveUsers = async () => {
+    return {
+        code: 200,
+        message: await db.User.findAll({
+            where: {
+                status: true
+            }
+        })
+    };
+}
 export default {
     createUser,
     getUserById,
     updateUser,
     deleteUser,
+    getActiveUsers,
+    findUsers,
+    bulkCreateUsers
 }
